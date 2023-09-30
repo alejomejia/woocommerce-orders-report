4e9a6af4c8ef5814146d2bae25c63c1f1c9d10f5
Alejandro Mejía <alejandro.mejia@bairesdev.com>
2023-09-30 15:08:43 -0500

    Add logs when starting the app

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import { EMAIL_RECEIVER } from './mailer/const.js';
 import { generateReport } from './generateReport.js';
 
 function App() {
+  logger.info('Starting app...');
+  logger.info('Waiting for cron job to be triggered...');
+
   scheduleJob('send-report', '59 23 * * *', async () => {
     await generateReport();
     logger.success(`Report generated and sent to: ${EMAIL_RECEIVER}`);
